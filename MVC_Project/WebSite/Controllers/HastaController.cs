decc58a2a96df1fc28261e861f00b0243cf64b0f
Bahadır Mustafa Erbay <bahadirmustafaerbay@gmail.com>
2018-03-21 00:05:35 +0300

    Initial Commit

---
+++ MVC_Project/WebSite/Controllers/HastaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class HastaController : Controller
+    {
+        // GET: Hasta
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
