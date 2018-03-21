a786cb3998884e6435dad8ac401704fa5083766a
Aslihan <aslipeker12@gmail.com>
2018-03-21 16:48:20 +0300

    Entity Framework Yüklendi ve Register sayfası oluşturuldu.

--- MVC_Project/WebSite/Controllers/HastaController.cs
+++ MVC_Project/WebSite/Controllers/HastaController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSite.Models;
 
 namespace WebSite.Controllers
 {
     public class HastaController : Controller
     {
+        HastaneDBEntities db = new HastaneDBEntities();
         // GET: Hasta
         public ActionResult Login()
         {
@@ -17,6 +19,20 @@ namespace WebSite.Controllers
         public ActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "hastaID,AD,SOYAD,ŞİFRE,ADRES,TELEFON,TC,DOĞUM_TARİHİ,CİNSİYET,kayıtTarihi")] hastalar hastalar)
+        {
+            if (ModelState.IsValid)
+            {
+                hastalar.kayıtTarihi = DateTime.Now;
+                db.hastalars.Add(hastalar);
+                db.SaveChanges();
+                return RedirectToAction("Login", "Hasta");
+            }
+
+            return View(hastalar);
         }
     }
 }
